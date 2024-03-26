5fcb127ce7fdb12da1feed4066c559fc1d74b889
Sarctiann (Sebastian Rodriguez) <sebastian.rodriguez@hstpathways.com>
2024-03-26 14:56:35 -0300

    updated keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,14 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("n", "<C-t>", "<cmd>Telescope<CR>", { desc = "Open Telescope" })
+
+-- Move Line (Mac Support).
+
+vim.keymap.set("n", "¶", "<cmd>m .+1<cr>==", { desc = "Move down" })
+vim.keymap.set("n", "§", "<cmd>m .-2<cr>==", { desc = "Move up" })
+
+vim.keymap.set("i", "¶", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+vim.keymap.set("i", "§", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+
+vim.keymap.set("v", "¶", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+vim.keymap.set("v", "§", ":m '<-2<cr>gv=gv", { desc = "Move up" })
