34f90ee80a3414f5e35a0e582e82b7d28ee05d4b
Sarctiann (Sebastian Rodriguez) <sebastian.rodriguez@hstpathways.com>
2024-03-28 12:55:47 -0300

    updated autosave config

--- lua/plugins/autosave.lua
+++ lua/plugins/autosave.lua
@@ -1,9 +1,15 @@
 return {
-  "okuuva/auto-save.nvim",
-  cmd = "ASToggle", -- optional for lazy loading on command
-  event = { "TextChanged" }, -- optional for lazy loading on trigger events
+  "okuuva/auto-save.nvim", -- [Docs](https://neovimcraft.com/plugin/okuuva/auto-save.nvim)
+  -- cmd = "ASToggle", -- optional for lazy loading on command
+  -- event = { "InsertLeave", "TextChange" }, -- optional for lazy loading on trigger events
+  enabled = true,
   opts = {
-    -- your config goes here
-    -- or just leave it empty :)
+    -- VSCode behavior
+    trigger_events = {
+      immediate_save = { "BufLeave" },
+      defer_save = { "FocusLost" },
+      cancel_defered_save = { "BufLeave" },
+    },
+    debounce_delay = 100,
   },
 }
