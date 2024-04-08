e3cca6d0a05d09cd862930728066236d5eaa5115
Sarctiann (Sebastian Rodriguez) <sebastian.rodriguez@hstpathways.com>
2024-04-08 10:54:01 -0300

    improved autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,15 +3,29 @@
 -- Add any additional autocmds here
 
 -- Set up custom cursor
-vim.api.nvim_create_autocmd({ "VimEnter", "GUIEnter", "BufEnter" }, {
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
   group = vim.api.nvim_create_augroup("Custom-Cursor", { clear = true }),
   callback = function()
     -- Set cursor colors
-    vim.api.nvim_command("highlight Cursor guibg=#aaff70")
-    vim.api.nvim_command("highlight ReplaceCursor guibg=#cc7070")
+    vim.api.nvim_command("highlight Cursor guibg=#aaffcc")
     vim.api.nvim_command("highlight VisualCursor guibg=#cc99cc")
+    vim.api.nvim_command("highlight ReplaceCursor guibg=#cc7070")
     -- Apply confs
-    vim.opt_local.cursorline = false
     vim.opt_local.guicursor = "n-c-ci-cr-sm:block,i:ver30-Cursor,v-ve-o:hor30-VisualCursor,r:hor50-ReplaceCursor"
+  end,
+})
+
+-- Add terminal transparency
+local FloatTransparency = vim.api.nvim_create_augroup("Custom-FloatTransparency", { clear = true })
+vim.api.nvim_create_autocmd({ "TermEnter" }, {
+  group = FloatTransparency,
+  callback = function()
+    vim.opt_local.winblend = 5
+  end,
+})
+vim.api.nvim_create_autocmd({ "TermLeave" }, {
+  group = FloatTransparency,
+  callback = function()
+    vim.opt_local.winblend = 0
   end,
 })
