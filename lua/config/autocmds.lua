155b184c8fe051fb8bddb7075ae0eb13d1948d45
Sarctiann <sebas.sarc@gmail.com>
2024-04-14 23:47:56 -0300

    adjusted Visual bgcolor

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,12 +3,13 @@
 -- Add any additional autocmds here
 
 -- Set up custom cursor
-vim.api.nvim_create_autocmd({ "BufEnter", "TermLeave" }, {
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
   group = vim.api.nvim_create_augroup("Custom-Cursor", { clear = true }),
   callback = function()
     -- Set cursor colors
     vim.api.nvim_command("highlight Cursor guibg=#aaffcc")
     vim.api.nvim_command("highlight VisualCursor guibg=#cc99cc")
+    vim.api.nvim_command("highlight Visual guibg=#300050")
     vim.api.nvim_command("highlight ReplaceCursor guibg=#cc7070")
     -- Apply confs
     vim.opt_local.guicursor = "n-c-ci-cr-sm:block,i:ver30-Cursor,v-ve-o:hor30-VisualCursor,r:hor50-ReplaceCursor"
@@ -21,7 +22,6 @@ vim.api.nvim_create_autocmd({ "TermEnter" }, {
   group = FloatTransparency,
   callback = function()
     vim.opt_local.winblend = 0
-    vim.opt_local.guicursor = "a:ver30"
   end,
 })
 vim.api.nvim_create_autocmd({ "TermLeave" }, {
