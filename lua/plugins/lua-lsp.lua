ec39c6cecf5a16b7e491ee8b42025508cebd1d50
Sarctiann (Sebastian Rodriguez) <sebastian.rodriguez@hstpathways.com>
2024-05-04 14:03:23 -0300

    updated lua lsp

--- lua/plugins/lua-lsp.lua
+++ lua/plugins/lua-lsp.lua
@@ -12,13 +12,13 @@ return {
           runtime = {
             -- Tell the language server which version of Lua you're using
             -- (most likely LuaJIT in the case of Neovim)
-            version = "LuaJIT",
+            version = "Lua",
           },
           -- Make the server aware of Neovim runtime files
           workspace = {
-            checkThirdParty = false,
+            checkThirdParty = true,
             library = {
-              vim.env.VIMRUNTIME,
+              -- vim.env.VIMRUNTIME,
               path,
             },
           },
