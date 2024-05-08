21bb5f216285ac9b13df8c4525d22fe3455ad21c
Sarctiann (Sebastian Rodriguez) <sebastian.rodriguez@hstpathways.com>
2024-05-08 10:14:48 -0300

    set off relativenumber

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.opt.relativenumber = false
+
 -- Set cursor colors
 vim.api.nvim_command("highlight CursorL guibg=#9ece6a")
 vim.api.nvim_command("highlight VisualCursor guibg=#bb9af7")
