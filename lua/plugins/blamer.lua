af32e6caea3ece8fe995bf320598bb04da8b6866
Sarctiann (Sebastian Rodriguez) <sebastian.rodriguez@hstpathways.com>
2024-05-09 15:19:31 -0300

    updated blamer

--- lua/plugins/blamer.lua
+++ lua/plugins/blamer.lua
@@ -3,4 +3,11 @@ return {
   config = function()
     vim.g.blamer_enabled = true
   end,
+  init = function()
+    vim.cmd([[
+      let g:blamer_prefix = '         '
+      let g:blamer_show_in_visual_modes = 0
+      let g:blamer_show_in_insert_modes = 0
+    ]])
+  end,
 }
