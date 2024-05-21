d262cbf1111be3acbf8aa050917d2bf35ff327b6
Sarctiann (Sebastian Rodriguez) <sebastian.rodriguez@hstpathways.com>
2024-05-21 11:08:30 -0300

    upgrade to nvim 0.10, and mojo config

--- lua/plugins/mojo-conf.lua
+++ lua/plugins/mojo-conf.lua
@@ -1,14 +1,13 @@
+require("nvim-web-devicons").set_icon({
+  mojo = {
+    icon = "󰈸",
+    color = "#d75f00",
+    cterm_color = "166",
+    name = "Mojo",
+  },
+})
+
 return {
-  "ovikrai/mojo-syntax",
-  config = function()
-    require("lspconfig").mojo.setup({})
-    require("nvim-web-devicons").set_icon({
-      mojo = {
-        icon = "󰈸",
-        color = "#d75f00",
-        cterm_color = "166",
-        name = "Mojo",
-      },
-    })
-  end,
+  "neovim/nvim-lspconfig",
+  opts = { servers = { mojo = {} } },
 }
