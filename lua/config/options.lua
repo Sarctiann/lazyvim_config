34d178b520e30321ff22eca7d40b1cd9f8f48e37
Sarctiann (Sebastian Rodriguez) <sebastian.rodriguez@hstpathways.com>
2024-05-30 13:16:49 -0300

    added colorcolumn

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,6 +7,8 @@ vim.opt.relativenumber = false
 -- Set cursor colors
 vim.api.nvim_command("highlight CursorL guibg=#9ece6a")
 vim.api.nvim_command("highlight VisualCursor guibg=#bb9af7")
--- Apply confs
 vim.api.nvim_command("highlight ReplaceCursor guibg=#f7768e")
+-- Apply confs
 vim.opt_local.guicursor = "n-c-ci-cr-sm:block,i:ver30-CursorL,v-ve-o:hor30-VisualCursor,r:hor50-ReplaceCursor"
+
+vim.opt.colorcolumn = "80"
