828723841ab057d31e1bf09a48e8f98fdd69bced
Sarctiann (Sebastian Rodriguez) <sebastian.rodriguez@hstpathways.com>
2024-06-24 16:57:04 -0300

    updated neo tree border style for popups

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,6 +1,12 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = function(_, opts)
-    opts.filesystem.filtered_items = { visible = true }
+    opts.popup_border_style = "rounded"
+    opts.filesystem = {
+      bind_to_cwd = false,
+      follow_current_file = { enabled = true },
+      use_libuv_file_watcher = true,
+      filtered_items = { visible = true },
+    }
   end,
 }
