04454ae841bbe2772c955358b6d88dacd4e88436
Sarctiann (Sebastian Rodriguez) <sebastian.rodriguez@hstpathways.com>
2024-09-16 11:30:06 -0300

    disabled minimap on start

--- lua/plugins/minimap.lua
+++ lua/plugins/minimap.lua
@@ -3,7 +3,7 @@ return {
   config = function()
     local codewindow = require("codewindow")
     codewindow.setup({
-      auto_enable = true,
+      auto_enable = false,
       minimap_width = 12,
       window_border = "rounded",
       exclude_filetypes = { "NvimTree", "minimap", "dashboard", "help" },
