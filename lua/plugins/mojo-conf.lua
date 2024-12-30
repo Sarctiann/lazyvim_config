a1087af51a4425db3b85e3bb1df42dc066ae1abc
Sarctiann (Sebastian Rodriguez) <sebastian.rodriguez@hstpathways.com>
2024-12-30 12:31:16 -0300

    updated

--- lua/plugins/mojo-conf.lua
+++ lua/plugins/mojo-conf.lua
@@ -3,11 +3,11 @@ return {
   opts = {
     servers = {
       mojo = {
-        -- This is required if you use magic-CLI instead of modular-CLI
+        -- this is required if you use magic-cli instead of modular-cli
         on_new_config = function()
           local result = vim.fn.system("magic shell")
           if vim.v.shell_error ~= 0 then
-            vim.notify("Error running `magic shell`: " .. result, vim.log.levels.ERROR)
+            vim.notify("error running `magic shell`: " .. result, vim.log.levels.error)
           end
         end,
       },
