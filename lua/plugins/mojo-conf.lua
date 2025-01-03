24455738645cc1b7c9f703b3e894908ff40e315e
Sarctiann (Sebastian Rodriguez) <sebastian.rodriguez@hstpathways.com>
2025-01-03 20:21:00 -0300

    updated mojo config

--- lua/plugins/mojo-conf.lua
+++ lua/plugins/mojo-conf.lua
@@ -5,15 +5,14 @@ return {
       mojo = {
         on_new_config = function(config)
           if vim.fn.executable("mojo-lsp-server") == 0 then
-            -- try to run `magic shell`
-            local result = vim.fn.system("magic shell")
-            if vim.v.shell_error ~= 0 then
-              vim.notify("LSP: " .. result, vim.log.levels.ERROR)
-              vim.notify("(Run `magic shell` before entering neovim)", vim.log.levels.INFO)
-              -- avoid trying to run mojo-lsp-server
-              config.cmd = { "echo", "" }
-              return
-            end
+            vim.notify(
+              "(Run `magic shell` before entering neovim)",
+              vim.log.levels.WARN,
+              { title = "MOJO LSP NOT STARTED", icon = "🚨", timeout = 10000 }
+            )
+            -- avoid trying to run mojo-lsp-server
+            config.cmd = { "echo", "" }
+            return
           end
         end,
       },
