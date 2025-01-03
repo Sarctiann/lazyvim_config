74a6a3c70b7ec860d3561df2d23d671debb6fa0a
Sarctiann <sebas.sarc@gmail.com>
2025-01-03 02:06:00 -0300

    updated mojo conf

--- lua/plugins/mojo-conf.lua
+++ lua/plugins/mojo-conf.lua
@@ -3,11 +3,17 @@ return {
   opts = {
     servers = {
       mojo = {
-        -- this is required if you use magic-cli instead of modular-cli
-        on_new_config = function()
-          local result = vim.fn.system("magic shell")
-          if vim.v.shell_error ~= 0 then
-            vim.notify("error running `magic shell`: " .. result, vim.log.levels.error)
+        on_new_config = function(config)
+          if vim.fn.executable("mojo-lsp-server") == 0 then
+            -- try to run `magic shell`
+            local result = vim.fn.system("magic shell")
+            if vim.v.shell_error ~= 0 then
+              vim.notify("LSP: " .. result, vim.log.levels.ERROR)
+              vim.notify("(Run `magic shell` before entering neovim)", vim.log.levels.INFO)
+              -- avoid trying to run mojo-lsp-server
+              config.cmd = { "echo", "" }
+              return
+            end
           end
         end,
       },
