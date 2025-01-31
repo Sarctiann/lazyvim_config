66bd2999fa79ef337d585c7f34446d938f038541
Sarctiann (Sebastian Rodriguez) <sebastian.rodriguez@hstpathways.com>
2025-01-31 16:13:55 -0300

    added vlang config

---
+++ lua/plugins/v-lang.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    optional = true,
+    opts = { ensure_installed = { "v-analyzer" } },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = { ensure_installed = { "v" } },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        v_analyzer = {},
+        -- vls = {}, -- you should have only one installed
+      },
+    },
+  },
+}
