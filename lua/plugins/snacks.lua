6b74a11260af142ec3650b5f225442778334aa7e
Sarctiann <sebas.sarc@gmail.com>
2025-04-26 22:52:15 -0300

    replaced dashboard with snacks

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,73 @@
+local logo = [[
+███████╗ █████╗ ██████╗  ██████╗     ██╗   ██╗██╗███╗   ███╗
+██╔════╝██╔══██╗██╔══██╗██╔════╝     ██║   ██║██║████╗ ████║
+███████╗███████║██████╔╝██║          ██║   ██║██║██╔████╔██║
+╚════██║██╔══██║██╔══██╗██║          ╚██╗ ██╔╝██║██║╚██╔╝██║
+███████║██║  ██║██║  ██║╚██████╗  ██╗ ╚████╔╝ ██║██║ ╚═╝ ██║
+╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═══╝  ╚═╝╚═╝     ╚═╝
+{greet}
+]]
+
+local sub_header = "\n\nWelcome " .. os.getenv("USER"):lower():gsub("^%l", string.upper) .. "!" .. "\n"
+
+return {
+  "snacks.nvim",
+  opts = {
+    dashboard = {
+      preset = {
+        pick = function(cmd, opts)
+          return LazyVim.pick(cmd, opts)()
+        end,
+        header = (logo:gsub("{greet}", sub_header)),
+        -- stylua: ignore
+        ---@type snacks.dashboard.Item[]
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
+      sections = {
+        { section = "header" },
+        { section = "keys", gap = 1, padding = 1 },
+        {
+          pane = 2,
+          icon = string.rep("\n", 8) .. string.rep(" ", 20) .. " ",
+          title = string.rep("\n", 8) .. "Git Status\n",
+          section = "terminal",
+          enabled = function()
+            return Snacks.git.get_root() ~= nil
+          end,
+          cmd = "git status --short --branch --renames",
+          height = 7,
+          padding = 1,
+          indent = 3,
+          ttl = 5 * 60,
+        },
+        {
+          pane = 2,
+          icon = string.rep(" ", 20) .. " ",
+          title = "Projects\n",
+          section = "projects",
+          indent = 2,
+          padding = 1,
+        },
+        {
+          pane = 2,
+          icon = string.rep(" ", 20) .. " ",
+          title = "Recent Files\n",
+          section = "recent_files",
+          indent = 2,
+          padding = 1,
+        },
+        { section = "startup" },
+      },
+    },
+  },
+}
