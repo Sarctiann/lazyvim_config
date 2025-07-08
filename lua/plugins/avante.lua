0f97595e566603523e687d045ea6d064277cbc6d
Sarctiann (Sebastian Rodriguez) <sebastian.rodriguez@hstpathways.com>
2025-07-08 11:08:55 -0300

    updated avante config

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,4 +1,26 @@
 return {
+  {
+    "saghen/blink.cmp",
+    dependencies = {
+      "Kaiser-Yang/blink-cmp-avante",
+      -- ... Other dependencies
+    },
+    opts = {
+      sources = {
+        -- Add 'avante' to the list
+        default = { "avante", "lsp", "path", "snippets", "buffer" },
+        providers = {
+          avante = {
+            module = "blink-cmp-avante",
+            name = "Avante",
+            opts = {
+              -- options for blink-cmp-avante
+            },
+          },
+        },
+      },
+    },
+  },
 
   {
     "ravitemer/mcphub.nvim",
@@ -48,30 +70,36 @@ return {
         end,
         provider = "copilot", -- Set provider to copilot
         disabled_tools = {
-          -- "list_files", -- Built-in file operations
-          -- "search_files",
-          -- "read_file",
-          -- "create_file",
-          -- "rename_file",
-          -- "delete_file",
-          -- "create_dir",
-          -- "rename_dir",
-          -- "delete_dir",
-          -- "bash", -- Built-in terminal access
+          "list_files", -- Built-in file operations
+          "search_files",
+          "read_file",
+          "create_file",
+          "rename_file",
+          "delete_file",
+          "create_dir",
+          "rename_dir",
+          "delete_dir",
+          "bash", -- Built-in terminal access
         },
       })
     end,
     dependencies = {
+      "ravitemer/mcphub.nvim",
+      "saghen/blink.cmp",
+
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
-      "ravitemer/mcphub.nvim",
+
       --- The below dependencies are optional,
-      "echasnovski/mini.pick", -- for file_selector provider mini.pick
-      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+
       "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-      "ibhagwan/fzf-lua", -- for file_selector provider fzf
-      "stevearc/dressing.nvim", -- for input provider dressing
+      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      -- "echasnovski/mini.pick", -- for file_selector provider mini.pick
+      -- "ibhagwan/fzf-lua", -- for file_selector provider fzf
+
       "folke/snacks.nvim", -- for input provider snacks
+      -- "stevearc/dressing.nvim", -- for input provider dressing
+
       "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
       "zbirenbaum/copilot.lua", -- for providers='copilot'
       {
