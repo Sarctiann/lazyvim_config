989ede17ec1e4bdc8e357526844962a4832098a3
Sarctiann (Sebastian Rodriguez) <sebastian.rodriguez@hstpathways.com>
2025-07-24 16:01:14 -0300

    removed unused code

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -5,32 +5,6 @@ return {
       inlay_hints = { enabled = false },
     },
   },
-  -- {
-  --   "saghen/blink.cmp",
-  --   opts = function(_, opts)
-  --     opts.keymap = { preset = "super-tab" }
-  --     opts.completion = {
-  --       accept = {
-  --         -- experimental auto-brackets support
-  --         auto_brackets = {
-  --           enabled = true,
-  --         },
-  --       },
-  --       menu = {
-  --         draw = {
-  --           treesitter = { "lsp" },
-  --         },
-  --       },
-  --       documentation = {
-  --         auto_show = true,
-  --         auto_show_delay_ms = 200,
-  --       },
-  --       ghost_text = {
-  --         enabled = false,
-  --       },
-  --     }
-  --   end,
-  -- },
   {
     "saghen/blink.cmp",
     version = not vim.g.lazyvim_blink_main and "*",
@@ -113,37 +87,6 @@ return {
     },
     ---@param opts blink.cmp.Config | { sources: { compat: string[] } }
     config = function(_, opts)
-      -- THE FOLLOWING DOESN?T WORK PROPERLY
-
-      -- -- setup compat sources
-      -- local enabled = opts.sources.default
-      -- for _, source in ipairs(opts.sources.compat or {}) do
-      --   opts.sources.providers[source] = vim.tbl_deep_extend(
-      --     "force",
-      --     { name = source, module = "blink.compat.source" },
-      --     opts.sources.providers[source] or {}
-      --   )
-      --   if type(enabled) == "table" and not vim.tbl_contains(enabled, source) then
-      --     table.insert(enabled, source)
-      --   end
-      -- end
-      --
-      -- -- add ai_accept to <Tab> key
-      -- if not opts.keymap["<Tab>"] then
-      --   if opts.keymap.preset == "super-tab" then -- super-tab
-      --     opts.keymap["<Tab>"] = {
-      --       require("blink.cmp.keymap.presets")["super-tab"]["<Tab>"][1],
-      --       LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
-      --       "fallback",
-      --     }
-      --   else -- other presets
-      --     opts.keymap["<Tab>"] = {
-      --       LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
-      --       "fallback",
-      --     }
-      --   end
-      -- end
-
       -- Unset custom prop to pass blink.cmp validation
       opts.sources.compat = nil
 
