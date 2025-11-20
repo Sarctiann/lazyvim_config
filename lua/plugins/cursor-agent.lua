ee14a6f4e6315b349cb9a72b7d48eedb02128760
Sarctiann (Sebastian Rodriguez) <sebastian.rodriguez@hstpathways.com>
2025-11-20 16:32:25 -0300

    release cursor-agent as an independent nvim plugin

--- lua/plugins/cursor-agent.lua
+++ lua/plugins/cursor-agent.lua
@@ -1,10 +1,16 @@
 return {
-  "Sarctiann/cursor-agent.nvim",
-  dependencies = {
-    "folke/snacks.nvim",
-  },
-  --- @type Cursor-Agent.Config
-  opts = {
-    use_default_mappings = true,
+  --- @module 'cursor-agent'
+  {
+    "Sarctiann/cursor-agent.nvim",
+    dependencies = {
+      "folke/snacks.nvim",
+    },
+    --- @type Cursor-Agent.Config
+    opts = {
+      use_default_mappings = true,
+      show_help_on_open = false,
+    },
+    dev = true,
+    dir = "~/Documents/SARCTIANN/LuaCode/custom_plugins/cursor-agent.nvim/",
   },
 }
