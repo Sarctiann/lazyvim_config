5535b62bd084ac0288ddf5a77d4556d2c747e3ee
Sarctiann (Sebastian Rodriguez) <sebastian.rodriguez@hstpathways.com>
2026-02-05 13:01:40 -0300

    updated cursor agent config

--- lua/plugins/cursor-agent.lua
+++ lua/plugins/cursor-agent.lua
@@ -13,8 +13,25 @@ return {
     opts = {
       use_default_mappings = true,
       show_help_on_open = true,
-      new_lines_amount = 2,
+      new_lines_amount = 1,
       window_width = 64,
+      open_mode = "plan",
+      cursor_window_keys = {
+        terminal_mode = {
+          normal_mode = { "<M-q>" },
+          insert_file_path = { "<C-p>" },
+          insert_all_buffers = { "<C-p><C-p>" },
+          new_lines = { "<CR>" },
+          submit = { "<C-s>" },
+          enter = { "<tab>" },
+          help = { "<M-?>", "??", "\\\\" },
+          toggle_width = { "<C-f>" },
+        },
+        normal_mode = {
+          hide = { "<Esc>" },
+          toggle_width = { "<C-f>" },
+        },
+      },
     },
     -- NOTE: Comment the two lines below to use the plugin from GitHub
     dev = true,
